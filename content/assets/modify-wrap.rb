ea856c929774a77c32694e0bc980f1b167d5d2b6
Mike Dalessio <mike@csa.net>
2014-06-21 16:35:52 -0400

    Cleaning up warnings.

--- content/assets/modify-wrap.rb
+++ content/assets/modify-wrap.rb
@@ -1,9 +1,7 @@
 require 'modify-setup'
-h1  = @doc.at_css "h1"
-div = @doc.at_css "div"
 # :startdoc:
 nodes = @doc.css "h1,div"
-wrapper = nodes.wrap("<div class='container'></div>")
+nodes.wrap("<div class='container'></div>")
 
 @doc.to_html
 # =>
