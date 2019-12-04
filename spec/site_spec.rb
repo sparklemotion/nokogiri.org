97358c54696f383ebc2b23646ca3a610d8bc801b
Mike Dalessio <mike.dalessio@gmail.com>
2019-12-04 16:12:02 -0500

    backfill tidelift expectations

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -16,4 +16,13 @@ describe "nokogiri.org" do
       expect(page.status_code).to eq(200)
     end
   end
+
+  feature "tidelift URLs" do
+    let(:tidelift_url) { "https://tidelift.com/subscription/pkg/rubygems-nokogiri?utm_source=rubygems-nokogiri&utm_medium=referral&utm_campaign=website" }
+
+    it "should link to tidelift from home page" do
+      visit "/"
+      expect(page).to have_link("Get support for Nokogiri with a Tidelift subscription", href: tidelift_url)
+    end
+  end
 end
