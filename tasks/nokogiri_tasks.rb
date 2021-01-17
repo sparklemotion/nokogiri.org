40c27f19a98b0f6f78dee4a34f4768e497298028
Mike Dalessio <mike.dalessio@gmail.com>
2021-01-17 17:04:41 -0500

    new task to clean up nokogiri rdocs

--- tasks/nokogiri_tasks.rb
+++ tasks/nokogiri_tasks.rb
@@ -1,8 +1,9 @@
 # coding: utf-8
+# frozen_string_literal: true
 RDOC_STAGING_DIR = File.join(STAGING_DIR, "rdoc")
 
 def create_nokogiri_tasks(source_dir, dest_dir)
-  FileUtils.mkdir_p dest_dir
+  FileUtils.mkdir_p(dest_dir)
 
   file_pairs = {
     "LICENSE.md" => "LICENSE.md",
@@ -22,26 +23,31 @@ def create_nokogiri_tasks(source_dir, dest_dir)
     dest_path = File.join(dest_dir, dest_file)
     dest_paths << dest_path
 
-    if !File.exist?(source_path)
+    unless File.exist?(source_path)
       raise "Could not find file #{source_path}, please set \$NOKOGIRI_DIR if necessary"
     end
 
-    file dest_path => source_path do
+    file(dest_path => source_path) do
       if dest_file == "index.md"
-        inject_tidelift_cta source_path, dest_path
+        inject_tidelift_cta(source_path, dest_path)
       else
-        FileUtils.cp source_path, dest_path, verbose: true
+        FileUtils.cp(source_path, dest_path, verbose: true)
       end
       modify_readme_links(dest_path)
     end
   end
 
-  desc "Generate and pull in Nokogiri rdocs"
-  task :rdoc do
+  desc("Generate and pull in Nokogiri rdocs")
+  task(:rdoc) do
     nokogiri_generate_rdocs
     nokogiri_add_ga_to_rdocs
   end
   dest_paths << "nokogiri:rdoc"
+
+  desc("Clean the Nokogiri rdocs")
+  task(:rdoc_clean) do
+    nokogiri_clean_rdocs
+  end
 
   dest_paths
 end
@@ -57,8 +63,12 @@ def nokogiri_generate_rdocs
 
   pwd = Dir.pwd
   Dir.chdir(nokogiri_dir) do
-    sh "yard doc --output-dir #{File.join(pwd, RDOC_STAGING_DIR)} --embed-mixins"
+    sh("yard doc --output-dir #{File.join(pwd, RDOC_STAGING_DIR)} --embed-mixins")
   end
+end
+
+def nokogiri_clean_rdocs
+  FileUtils.rm_rf(RDOC_STAGING_DIR, verbose: true)
 end
 
 def nokogiri_add_ga_to_rdocs
@@ -86,7 +96,7 @@ def nokogiri_add_ga_to_rdocs
     puts "→ adding google analytics snippet to #{file_path}"
     doc = Nokogiri::HTML.parse(html)
     doc.at_css("head").add_child(snippet)
-    File.open(file_path, "w") { |f| f.write doc.to_html }
+    File.open(file_path, "w") { |f| f.write(doc.to_html) }
   end
 end
 
@@ -95,16 +105,16 @@ def modify_readme_links(dest_path)
   modified_contents = contents.gsub(/\]\(README.md\)/, "](index.md)")
   if contents != modified_contents
     puts "→ repointing a README.md link in #{dest_path}"
-    File.open(dest_path, "w") { |f| f.write modified_contents }
+    File.open(dest_path, "w") { |f| f.write(modified_contents) }
   end
 end
 
 def inject_tidelift_cta(source_path, dest_path)
   puts "→ adding tidelift cta into #{source_path} → #{dest_path}"
   File.open(dest_path, "w") do |dest_fd|
-    dest_fd.write tidelift_cta
+    dest_fd.write(tidelift_cta)
     dest_fd.puts
-    dest_fd.write File.read(source_path)
+    dest_fd.write(File.read(source_path))
   end
 end
 
